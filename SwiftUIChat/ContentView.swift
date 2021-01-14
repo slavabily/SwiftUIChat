73b1f586f04464ed3404ab21767b8802d282dd97
slava bily <slavabily@me.com>
2021-01-14 21:01:38 +0200

    Initial Commit

---
+++ SwiftUIChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChat
+//
+//  Created by slava bily on 14.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
